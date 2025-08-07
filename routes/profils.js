fdc976dde448673e70519bc189b3440b77fa30fc
Tim Della-Chiesa Pasquer <tim.dellachiesapasquer@gmail.com>
2025-08-07 12:38:17 +0200

    ajout début algo pour profil swip

--- routes/profils.js
+++ routes/profils.js
@@ -6,6 +6,7 @@ const User = require("../models/users");
 const Conversation = require("../models/conversations");
 const { authenticateToken } = require("../modules/jwt");
 const { body, validationResult } = require("express-validator");
+const mongoose = require('mongoose');
 
 //_________________________________________________________ENVOYER DES PROFILS_______________________________________________________________
 
@@ -25,12 +26,39 @@ function deg2rad(deg) {
 
 router.get("/profil", authenticateToken, async (req, res) => {
 	try {
-		const user = await User.findById(req.userId);
-		const data = await User.find({valid: true}).select("username birthdate gender orientation relationship photoList latitude longitude tastesList").limit(10);
+		const user = await User.findById(req.userId).populate('proposedList');
+    if (user.proposedList && user.proposedList.length !== 0) {
+      const result = [];
+      for (const element of user.proposedList) {
+        const { _id, username, birthdate, gender, orientation, relationship, photoList, latitude, longitude, tastesList } = element;
+        if (String(_id) === String(req.userId)) {
+          continue;
+        }
+        const distance = `${Math.ceil(getDistanceFromLatLonInKm(user.latitude, user.longitude, latitude, longitude))} km`;
+        result.push({ _id, username, birthdate, gender, orientation, relationship, photoList, distance, tastesList });
+      }
+      res.json({ result: true, profilList: result });
+      return;
+    }
+    const data = await User.aggregate([
+      {
+        $match: {
+          valid: true,
+        }
+      },
+      {
+        $sample: {
+          size: 10
+        }
+      }
+    ]);
 		const result = [];
 		for (const element of data) {
 			const { _id, username, birthdate, gender, orientation, relationship, photoList, latitude, longitude, tastesList } = element;
       if (String(_id) === String(req.userId)) {
+        continue;
+      }
+      if (user.likesList.some((x) => String(x) === String(_id)) || user.superlikesList.some((x) => String(x) === String(_id))) {
         continue;
       }
 			const distance = `${Math.ceil(getDistanceFromLatLonInKm(user.latitude, user.longitude, latitude, longitude))} km`;
@@ -51,6 +79,10 @@ router.put("/swipe", authenticateToken, body("action").isString(), body("userId"
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ result: false, error: errors.array() });
 		}
+    const user = await User.findById(req.userId);
+    if (!user.proposedList.some(x => String(x) = String(req.userId))) {
+      return res.json({result: false, error: 'Profil non proposé'});
+    }
 		const otherUser = await User.findById(req.body.userId);
 		if (!otherUser) {
 			res.status(403).res.json({ result: false, error: "Profil non trouvé" });
@@ -58,10 +90,10 @@ router.put("/swipe", authenticateToken, body("action").isString(), body("userId"
 		}
 		if (req.body.action.toLowerCase() === "like") {
 			const data = await User.findByIdAndUpdate(req.userId, {
-				$push: { likesList: req.body.userId },
+				$push: { likesList: req.body.userId }, $pull: {proposedList: new mongoose.Types.ObjectId(req.body.userId)}  
 			});
 			let match = false;
-			if (otherUser.likesList.some((x) => String(x) === String(req.userId))) {
+			if (otherUser.likesList.some((x) => String(x) === String(req.userId)) || otherUser.superlikesList.some((x) => String(x) === String(req.userId))) {
 				match = true;
 				const newConversation = new Conversation({
 					user1: req.userId,
@@ -69,20 +101,20 @@ router.put("/swipe", authenticateToken, body("action").isString(), body("userId"
 					messageList: [],
 				});
 				const conv = await newConversation.save();
-				await User.findByIdAndUpdate(req.userId, { $push: { conversationList: conv._id } });
-				await User.findByIdAndUpdate(req.body.userId, { $push: { conversationList: conv._id } });
+				await User.findByIdAndUpdate(req.userId, { $push: { conversationList: conv._id }});
+				await User.findByIdAndUpdate(req.body.userId, { $push: { conversationList: conv._id }});
 			}
 			res.json({ result: true, likesList: data, match });
 			console.log("Le profil a été liké !");
 		} else if (req.body.action.toLowerCase() === "superlike") {
 			const data = await User.findByIdAndUpdate(req.userId, {
-				$push: { superlikesList: req.body.userId },
+				$push: { superlikesList: req.body.userId }, $pull: {proposedList: new mongoose.Types.ObjectId(req.body.userId)}  
 			});
 			res.json({ result: true, superlikesList: data });
 			console.log("Le profil a été superliké !");
 		} else {
 			const data = await User.findByIdAndUpdate(req.userId, {
-				$push: { dislikesList: req.body.userId },
+				$push: { dislikesList: req.body.userId }, $pull: {proposedList: new mongoose.Types.ObjectId(req.body.userId)}  
 			});
 			res.json({ result: false, dislikesList: data });
 			console.log("Le profil a été disliké !");
