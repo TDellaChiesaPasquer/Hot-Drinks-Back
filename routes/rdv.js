79e4a026e36ed62098ac4e302bbeff21d8535bed
Caroline Weinberger <caroline.weinberger04@gmail.com>
2025-08-12 09:23:21 +0200

    matin

--- routes/rdv.js
+++ routes/rdv.js
@@ -8,11 +8,10 @@ const User = require("../models/users");
 const { authenticateToken } = require("../modules/jwt");
 
 router.put("/ask", authenticateToken, async (req, res) => {
-  const requete = await fetch(
-    `https://us1.locationiq.com/v1/reverse?key=${process.env.EXPO_PUBLIC_TOKEN}&lat=${req.body.latitude}&lon=${req.body.longitude}&format=json&`
-  );
   try {
-    // if (req.status === demande) {
+    const requete = await fetch(
+      `https://us1.locationiq.com/v1/reverse?key=${process.env.EXPO_PUBLIC_TOKEN}&lat=${req.body.latitude}&lon=${req.body.longitude}&format=json&`
+    );
     const data = await requete.json();
     //console.log(data);
     const coordinateRdv = {
@@ -22,9 +21,17 @@ router.put("/ask", authenticateToken, async (req, res) => {
       city: data.address.city,
       country: data.address.country,
     };
+    const newRdv = new Rdv({
+      status: req.body.status,
+      address: req.body.address,
+      date: req.body.date,
+    });
+    const rdv = await newRdv.save();
     console.log("ici");
-    res.json({ result: true, location: coordinateRdv });
-    // }
+    await User.findByIdAndUpdate(req.body.creator, {
+      $push: { rdvList: rdv._id },
+    });
+    res.json({ result: true, location: coordinateRdv, rdv: newRdv });
   } catch (error) {
     console.log(error);
     res.json({ result: false, error: "Server error" });
@@ -33,15 +40,15 @@ router.put("/ask", authenticateToken, async (req, res) => {
 
 router.put("/rdv/reponse", authenticateToken, async (req, res) => {
   try {
-    // if (req.body.status === confirmé) {
-    //   const data = await User.findByIdAndUpdate(req.userId, {
-    //     $push: { rdvList: req.body.userId },
-    //     $pull: { rdvList: new mongoose.Types.ObjectId(req.body.userId) },
-    //   });
-    //   res.json({ result: true, superlikesList: data });
-    //   console.log("Le profil a été superliké !");
-    //   res.json;
-    // }
+    if (req.body.status === confirmé) {
+      const data = await User.findByIdAndUpdate(req.userId, {
+        $push: { rdvList: req.body.userId },
+        $pull: { rdvList: new mongoose.Types.ObjectId(req.body.userId) },
+      });
+      res.json({ result: true, rdvList: data });
+      console.log("Le rdv a été confirmé !");
+      res.json;
+    } else req.body.status === refusé;
   } catch (error) {
     console.log(error);
     res.status(500).json({ result: false, error: "Server error" });
